8279c5cd9f9d9d52cee0cbf9016264a08f5a42e5
PChalmers <pchalmers2011@gmail.com>
2014-12-16 15:30:50 -0500

    Adding data files

---
+++ diffTool/src/diffTool/diffTool.java
@@ -0,0 +1,25 @@
+package diffTool;
+
+public class diffTool {
+
+	public void run() {
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+
+			diffTool obj = new diffTool();
+			obj.run();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Usage: ...");
+		}
+
+	}
+
+}
