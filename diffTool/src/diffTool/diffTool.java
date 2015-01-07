a9e4e2a5adabb24ac540da2ae8808c9439e24ee1
PChalmers <pchalmers2011@gmail.com>
2015-01-07 16:05:02 -0500

    Trying to create runnable jar file

--- diffTool/src/diffTool/diffTool.java
+++ diffTool/src/diffTool/diffTool.java
@@ -14,7 +14,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
