bb5676ad179b99aad38902acb3cfabdbb7777120
PChalmers <pchalmers2011@gmail.com>
2016-09-15 11:55:23 -0400

    Added javax.ejb-api.jar

---
+++ PaulEJB1/ejbModule/sample/firsst.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class firsst
+ */
+@Stateless
+@LocalBean
+public class firsst {
+
+    /**
+     * Default constructor. 
+     */
+    public firsst() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
